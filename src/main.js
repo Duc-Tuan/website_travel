420c3b742440ab973888e7f8829af0fb6e9930a1
Tuan <phamductuan26032002@gmail.com>
2022-04-20 23:12:44 +0700

    initial commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
