73aa08ec487a81f7caa345e8a8a30cbf85cb57db
Tuan <phamductuan26032002@gmail.com>
2022-04-20 23:51:38 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,15 +1,26 @@
 'use strict';
+const path = require('path');
 
 const express = require('express');
+const hbs = require('express-handlebars');
 const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
 app.use(morgan('combined'));
 
+// Template engine
+app.engine('.hbs', hbs.engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('home');
 });
+
+app.get('/news', (req, res) => {
+    res.render('news');
+  });
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
